cb5c94361a2f2e36d3a8b0474ee2c3c4d404ee8d
dangchien-jpg <nguyendangchien12.5.a@gmail.com>
2024-11-29 08:16:59 +0700

    baitapcannhan

--- app/src/main/java/com/example/nguyendangchien_btapcanhan/MainActivity2.java
+++ app/src/main/java/com/example/nguyendangchien_btapcanhan/MainActivity2.java
@@ -2,33 +2,38 @@ package com.example.nguyendangchien_btapcanhan;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Button  button = findViewById(R.id.sign_in);
-        button.setOnClickListener(new View.OnClickListener() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-                startActivity(intent);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if( item.getItemId() == R.id.account){
+                    Intent intent = new Intent( MainActivity2.this, MainActivity3.class);
+                    startActivity(intent);
+                }
+                return false;
             }
         });
     }
